a9e29d8fe9f33ee4dc5249cb91b213e5504277dd
Ana Solis Garcia <anasolis1999@gmail.com>
2021-10-28 16:46:50 +0200

    28-10-2021: Añadimos el source para el fichero del cual sacamos las funciones que vamos a utilizar

--- FuncionHurwicz.R
+++ FuncionHurwicz.R
@@ -1,7 +1,7 @@
 ###### Función para aplicar el Método de Hurwicz que nos permita obtener los valores de alpha para los que cambian las alternativas óptimas
 
 ## Funciones que vamos a utilizar del fichero "teoriadecision_funciones_incertidumbre.R" (he copiado y pegado)
-
+source("teoriadecison_funciones_incertidumbre.R")
 criterio.Hurwicz = function(tablaX,alfa=0.3,favorable=TRUE) {
   # alfa es un escalar entre 0 y 1 lo obtiene para ese único valor
   X = tablaX;
